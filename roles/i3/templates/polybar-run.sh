6e8ddf9f1166a0fe74c4b5f234c1960512adc0af
Dougal Matthews <dougal.matthews@aiven.io>
2022-05-03 12:25:30 +0100

    Add other times

--- roles/i3/templates/polybar-run.sh
+++ roles/i3/templates/polybar-run.sh
@@ -7,13 +7,5 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 
-if [ $(xrandr --listmonitors | wc -l) == "2" ];
-then
-    polybar PolybarTop -r &
-    polybar PolybarBot -r &
-else
-    polybar PolybarTop -r &
-    polybar PolybarBot -r &
-    polybar PolybarTopLeft -r &
-    polybar PolybarTopRight -r &
-fi
+polybar PolybarTop -r &
+polybar PolybarBot -r &
